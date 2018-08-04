d9e5f27c1f0a53d8713aec0a322cc96107885176
^[USERNAME]^ <aviv1357924680@walla.com>
2018-08-04 22:50:02 +0300

    Added a 'smarter' function, still a WIP

--- Set1/find_1byte_xor_key.py
+++ Set1/find_1byte_xor_key.py
@@ -1,10 +1,26 @@
 
 import string
-from collections import Counter
+import collections
 
 # Returns empty string if something failed.
 
-def decrypt(xored_text):
+# Change logic to first do most common check and only then check if one out of the three most common is space.
+
+def decrypt_1byte_new(xored_text): # Still a WIP, fix to find the string in next challenge
+    s = ""
+    byte_count = collections.Counter(xored_text)
+    common = byte_count.most_common(3)
+    
+    for i in xrange(len(common)):
+        diff = (ord(common[i][0]) ^ ord(' '))
+        s = "".join([chr(ord(x)^diff) for x in xored_text])
+        if chr(ord(common[0][0])^diff) == ' ' and (chr(ord(common[1][0])^diff).lower() in 'aeiou' or chr(ord(common[2][0])^diff).lower() in 'aeiou'):
+            return s
+    
+    return ""
+            
+
+def decrypt_1byte_old(xored_text):
     for item in xrange(1, 256):
         s = ""
         for index in xrange(len(xored_text)):
