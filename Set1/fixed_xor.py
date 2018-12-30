e4668eef7305233408bf7f319f75c3a7456f7f04
^[USERNAME]^ <aviv1357924680@walla.com>
2018-12-30 05:32:52 +0200

    changed one liner to a loop for convenience of reading

--- Set1/fixed_xor.py
+++ Set1/fixed_xor.py
@@ -1,10 +1,12 @@
 
 # Returns empty string if length if strings length isn't the same.
 def xor_strings(data1, data2):
-	if len(data1) != len(data2):
-		return "";
-		
-	data1 = data1.decode('hex')
-	data2 = data2.decode('hex')
-		
-	return "".join([chr(ord(data1[i])^ord(data2[i])) for i in xrange(len(data1))])
+    if len(data1) != len(data2):
+        return "";
+        
+    s = ""
+    
+    for i in xrange(len(data1)):
+        s += chr(ord(data1[i]) ^ ord(data2[i]))
+        
+    return s
