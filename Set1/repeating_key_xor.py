378400c41913c4b5cee0aad09f5e6050f9597b53
^[USERNAME]^ <aviv1357924680@walla.com>
2018-12-30 08:16:43 +0200

    first upload

---
+++ Set1/repeating_key_xor.py
@@ -0,0 +1,12 @@
+
+
+KEY = "ICE"
+
+
+def repeat_xor(text_to_xor):
+    s = ""
+    index = 0
+    for item in xrange(len(text_to_xor)):
+        s += chr(ord(text_to_xor[index]) ^ ord(KEY[item % len(KEY)]))
+        index += 1
+    print s.encode('hex')
